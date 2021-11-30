347bd37357bff71765295e3cfb92798e09eab36f
withColinSong <withColinSong@gmail.com>
2021-11-30 16:42:15 +0900

    add: jap 등록

---
+++ study/ex1-hello-jap/src/main/java/com/hellojpa/JpaMain.java
@@ -0,0 +1,31 @@
+package com.hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction tx = em.getTransaction(); // 트랜잭션
+        tx.begin();
+
+        try {
+
+            Member member = new Member();
+            member.setId(1L);
+            member.setName("HelloA");
+            em.persist(member);
+            tx.commit();
+        } catch(Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+}
