c0338bf8d07d71660f7d0e281d98f20e9c311c9a
withColinSong <withColinSong@gmail.com>
2021-11-30 19:20:45 +0900

    add: 영속성

--- study/ex1-hello-jap/src/main/java/com/hellojpa/JpaMain.java
+++ study/ex1-hello-jap/src/main/java/com/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -16,9 +17,11 @@ public class JpaMain {
 
         try {
 
-            Member findMember = em.find(Member.class, 1L);
-            System.out.println("findMember.id = " + findMember.getId());
-            System.out.println("findMember.name = " + findMember.getName());
+            Member member = em.find(Member.class, 150L);
+            member.setName("test01");
+            System.out.println("=========================");
+
+            System.out.println(member.getName());
             tx.commit();
         } catch(Exception e) {
             tx.rollback();
