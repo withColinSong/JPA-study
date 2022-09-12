f12ed62c6dffbde89742b6bdeb19be0e216326dc
withColinSong <withColinSong@gmail.com>
2022-09-12 21:40:29 +0900

    Revert "fix commit fc87b79"
    This reverts commit fc87b79624beead37fb01442d96318ac2546085d.

--- 02.jpa-application/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ 02.jpa-application/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,7 +1,6 @@
 package jpabook.jpashop.domain.item;
 
 
-import jpabook.jpashop.domain.exception.NotEnoughStockException;
 import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
@@ -23,33 +22,6 @@ public abstract class Item {
     private String name;
     private int price;
     private int stockQuantity;
-
-
-    // 비지니스 로직
-    // 데이터 로직에 있을 시 바람직하다.
-    // setter를 만드는게 아니라 메서드를 추가해서 메서드의 비지니스 로직을 추가하는게 좋다.
-
-    /**
-     * stock 증가
-     * @param quantity
-     */
-    public void addStock(int quantity) {
-        this.stockQuantity += quantity;
-    }
-
-    /**
-     * stock 감소
-     * 0보다 작을 때 Exception 처리
-     * @param quantity
-     */
-    public void removeStock(int quantity) {
-        int restStock = this.stockQuantity - quantity;
-        if(restStock < 0) {
-            throw new NotEnoughStockException("need more stock");
-        }
-
-        this.stockQuantity = restStock;
-    }
 
 
 }
