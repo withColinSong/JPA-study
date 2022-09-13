cba6952414550b38a054b7d03d251168a5157749
withColinSong <withColinSong@gmail.com>
2022-09-13 22:47:40 +0900

    add : #13 주문, 주문상품 엔티티 비지니스 로직 추가

--- 02.jpa-application/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
+++ 02.jpa-application/jpashop/src/main/java/jpabook/jpashop/domain/Order.java
@@ -1,7 +1,10 @@
 package jpabook.jpashop.domain;
 
+import jpabook.jpashop.domain.enums.DeliveryStatus;
 import jpabook.jpashop.domain.enums.OrderStatus;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -14,6 +17,7 @@ import java.util.List;
 @Setter
 @Table(name = "orders")
 @Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Order {
 
     @Id
@@ -35,7 +39,7 @@ public class Order {
     private LocalDateTime orderDate;
 
     @Enumerated(EnumType.STRING)
-    private OrderStatus orderStatus;
+    private OrderStatus status;
 
     // 연관관계 메서드
     public void setMember(Member member) {
@@ -54,6 +58,42 @@ public class Order {
     }
 
 
+    // 생성 메서드
+    public static Order createOrder(Member member, Delivery delivery, OrderItem... orderItems) {
+        Order order = new Order();
+        order.setMember(member);
+        order.setDelivery(delivery);
+        for(OrderItem orderItem : orderItems) {
+            order.addOrderItem(orderItem);
+        }
+
+        order.setStatus(OrderStatus.ORDER);
+        order.setOrderDate(LocalDateTime.now());
+        return order;
+    }
+
+    // 비지니스 로직
+
+    // 주문 취소
+    public void cancel() {
+        if(delivery.getStatus() == DeliveryStatus.COMP) {
+            throw new IllegalStateException("이미 배송완료된 상품은 취소가 불가능합니다.");
+        }
+
+        this.setStatus(OrderStatus.CANCEL);
+        for(OrderItem orderItem : orderItems) {
+            orderItem.cancel();
+        }
+    }
+
+    // 전체 주문 가격 조회
+    public int getTotalPrice() {
+        int totalPrice = 0;
+        for(OrderItem orderItem : orderItems) {
+            totalPrice += orderItem.getTotalPrice();
+        }
+        return totalPrice;
+    }
 
 
 
