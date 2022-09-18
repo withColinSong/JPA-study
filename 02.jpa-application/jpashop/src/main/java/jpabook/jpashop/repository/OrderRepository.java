e942177e8ad83a71a5e5fbf9012c1de9b9e7316b
withColinSong <withColinSong@gmail.com>
2022-09-18 19:58:15 +0900

    add :  #14 상품 주문 목록 및 취소 기능 추가

--- 02.jpa-application/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ 02.jpa-application/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -21,7 +21,7 @@ public class OrderRepository {
         return em.find(Order.class, id);
     }
 
-    public List<Order> findAll(OrderSearch orderSearch) {
+    public List<Order> findAllByString(OrderSearch orderSearch) {
         return em.createQuery("select o from Order o join o.member m" +
                         " where o.status = : status" +
                         " and m.name like :name", Order.class)
